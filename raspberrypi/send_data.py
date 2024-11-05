d8802cc67a4ae4ec13d5249ebb27f8519b3f7137
Evan-OR <tehEvster@gmail.com>
2024-11-05 14:32:43 +0000

    script now sends live gyro data

--- raspberrypi/send_data.py
+++ raspberrypi/send_data.py
@@ -19,7 +19,7 @@ sio.connect(args.u)
 while True:
     temperature_data = {
         "tempData": [datetime.now().isoformat(), s.get_temperature()],
-        "gyroData": {'x': 0.06546039879322052, 'y': 0.21827052533626556, 'z': -1.0255765914916992}
+        "gyroData": s.get_accelerometer_raw()
     }
     sio.emit('receive_data', temperature_data)
     print("Sent message:", temperature_data)
